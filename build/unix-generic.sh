853e98151fd2fb4dcc74dabe1e01c012c8f0746f
SSE4 <tomskside@gmail.com>
2013-11-24 00:22:22 +0700

    add code coverage (gcov)

--- build/unix-generic.sh
+++ build/unix-generic.sh
@@ -2,8 +2,8 @@
 WORKDIR=$1
 COMPILER=$2
 FLAGS="-Wall -std=c99"
-rm -rf $1
-mkdir $1
-cp CMakeLists.txt $1/CMakeLists.txt
+rm -rf $WORKDIR
+mkdir $WORKDIR
+cp CMakeLists.txt $WORKDIR/CMakeLists.txt
 cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=$COMPILER -DCMAKE_C_FLAGS="$FLAGS" $WORKDIR/CMakeLists.txt
-(cd $1 && make)
+(cd $WORKDIR && make)
