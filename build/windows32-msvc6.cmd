9dc71cbb5144bc24e6bef0d98e71c309c32ab956
SSE4 <tomskside@gmail.com>
2014-02-16 22:38:26 +0700

    use environment variable %MSDEVDIR% for Microsoft Visual Studio 6

--- build/windows32-msvc6.cmd
+++ build/windows32-msvc6.cmd
@@ -2,7 +2,7 @@ rmdir /s/q windows32-msvc6
 mkdir windows32-msvc6
 cd windows32-msvc6
 cmake -G "Visual Studio 6" -DCMAKE_C_FLAGS="/W4 /Zm200" ..
-"%VS60COMNTOOLS%\..\MSDev98\Bin\msdev.exe" DisAsm.dsw /MAKE "ALL_BUILD - Win32 Release"
+"%MSDEVDIR%\Bin\msdev.exe" DisAsm.dsw /MAKE "ALL_BUILD - Win32 Release"
 Release\DisAsmTest.exe
 Release\DisAsmBenchmark.exe %IPP_HOME%\ipp.bin
 cd ..
